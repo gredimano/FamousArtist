82690d28b33e052d336fd2f94932ec33085f49a5
Generoso Redimano Jr <gredimano@objectbright.com>
2018-02-04 15:48:01 +0800

    Create user form.

--- app/src/main/java/com/redredimano/famousartist/CreateListFormActivity.kt
+++ app/src/main/java/com/redredimano/famousartist/CreateListFormActivity.kt
@@ -2,11 +2,20 @@ package com.redredimano.famousartist
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_create_list_form.*
 
 class CreateListFormActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_list_form)
+    }
+
+    fun submitButtonClicked(view: View) {
+        Log.i("CreateUser", "firstName: ${et_firstName.text}")
+        Log.i("CreateUser", "lastName: ${et_lastName.text}")
+        Log.i("CreateUser", "email: ${et_email.text}")
     }
 }
